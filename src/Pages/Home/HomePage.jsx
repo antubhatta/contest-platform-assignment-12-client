dd1c4b23ef0dadc863cc47c66abf3a4050a10f36
antubhatta <antubhatta09@gmail.com>
2023-12-05 02:01:06 +0600

    assignment updated successfully

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AboutUsSection from "./AboutUs";
 import Banner from "./Banner";
 import BestCreators from "./BestCreators";
@@ -5,12 +6,20 @@ import PopularContests from "./PopularContests";
 import ReviewSection from "./ReviewSection";
 import WhyChooseUs from "./WhyChooseUs";
 import Winners from "./Winners";
+import usePopularContests from "../../hooks/usePopularContests";
 
 const HomePage = () => {
+  const [text, setText] = useState("");
+  const { refetch } = usePopularContests(text);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    refetch();
+  };
   return (
     <div>
-      <Banner />
-      <PopularContests />
+       <Banner handleSearch={handleSearch} text={text} setText={setText} />
+      <PopularContests text={text} />
       <Winners />
       <BestCreators />
       <AboutUsSection></AboutUsSection>
